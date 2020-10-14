ba9aa951300cc4d8496023ea60dfc1c37398480f
et4891 <et1103@hotmail.com>
2020-10-13 20:47:48 -0700

    added delete, NOTE: axios need to downgrade else axios.delete will not work

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,10 +1,12 @@
 import axios from "axios";
-const BASE_URL = "https://express-tutorial-api.herokuapp.com/api/todo";
+// const BASE_URL = "https://express-tutorial-api.herokuapp.com/api/todo";
+const BASE_URL = "http://localhost:3001/api/todo";
 
 export default {
   fetch: () => {
     return axios.get(BASE_URL);
   },
   create: item => axios.post(BASE_URL, { item }),
+  remove: _id => axios.delete(BASE_URL, { data: { _id } }),
   isCompleted: todo => axios.patch(`${BASE_URL}/completed`, todo)
 };
